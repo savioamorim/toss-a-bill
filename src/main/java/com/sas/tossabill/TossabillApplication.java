a445ea087e5f0c87a63eda0922c0e7a6a427d95d
Savio Amorim <savioamorim.s@hotmail.com>
2020-08-02 19:16:54 -0300

    Subir projeto para o repositorio git

---
+++ src/main/java/com/sas/tossabill/TossabillApplication.java
@@ -0,0 +1,13 @@
+package com.sas.tossabill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossabillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TossabillApplication.class, args);
+	}
+
+}
